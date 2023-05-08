17ec4d8dbda9d694d408c7dc4f5483c79c2cb018
İsmail Harmanda <iharmanda@hotmail.com>
2023-05-08 19:19:54 +0300

    connect height and label sliders IBOutlets and onPress calculate button IBAction

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     @IBAction func heightSliderChanged(_ sender: UISlider) {
         let height=String(format: "%.2f", sender.value)
         heightLabel.text="\(height)m"
@@ -29,6 +32,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        print(heightSlider.value,weightSlider.value)
+    }
+    
 }
 
