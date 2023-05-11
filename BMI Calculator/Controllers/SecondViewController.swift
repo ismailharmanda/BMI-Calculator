e1e5d4bbfbfdf8ed835e44c545e98b8b6fe7f30a
İsmail Harmanda <iharmanda@hotmail.com>
2023-05-11 09:39:51 +0300

    create a new view controller

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by ismail harmanda on 11.05.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+
+import UIKit
+
+
+class SecondViewController: UIViewController {
+    
+}
